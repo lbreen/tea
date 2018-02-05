62d5285b08e84318d13cee965a4c47e476bca142
Luke Breen <l.breen1@outlook.com>
2018-02-05 23:24:28 +0000

    Cannot create GroupMember unless the user's are friends

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -2,16 +2,16 @@ class GroupMember< ApplicationRecord
   belongs_to :member, class_name: "User", foreign_key: "user_id"
   belongs_to :group
 
-  # before_create :users_are_friends
+  before_create :users_are_friends
 
-  # def users_are_friends
-  #   admin = self.group.admin
-  #   member = self.member
+  def users_are_friends
+    admin = self.group.admin
+    member = self.member
 
-  #   if member.accepted_friends.include? admin == false
-
-  #   else
-
-  #   end
-  # end
+    if member.accepted_friends.include? admin
+      return true
+    else
+      raise "You can only invite friends to your group."
+    end
+  end
 end
