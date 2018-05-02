a3aaecc8c12c806cea0128107149354d587bbb7c
Luke Breen <l.breen1@outlook.com>
2018-05-02 19:55:33 +0100

    As a member of a group, i can create and read a message for that group

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,27 @@
 class MessagesController < ApplicationController
+  before_action :find_group, only: [ :create ]
+
   def create
-    message = Message.new(message_params)
-    message.user = current_user
-    if message.save
+    @message = Message.new(message_params)
+    binding.pry
+    @message.group = @group
+    @message.user = current_user
+    authorize @message
+    if @message.save
       ActionCable.server.broadcast 'messages',
-        message: message.content,
-        user: message.user.username
+        message: @message.content,
+        user: @message.user.first_name
       head :ok
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def find_group
+    @group = Group.find(params[:group_id])
   end
 end
